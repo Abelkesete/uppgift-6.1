3564926ea59061adad9c5d32ccaf155a80f48798
Elev <Elev@TEB125123>
2023-03-03 14:30:15 +0100

    Add project files.

---
+++ uppgift 6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace uppgift
+{
+    class Ptogram
+    {
+        static void Main(string[] args)
+        {
+
+            add(5, 3, 10);
+            Console.WriteLine(add(5, 3, 10));
+           
+        }
+        static int add(int tal1, int tal2, int tal3)
+        {
+            return (tal1 + tal2 + tal3);
+        }
+
+    }
+}
